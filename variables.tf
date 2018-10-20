3be0ca2fd0102cefaa011fb0fd86849256c51117
Eimert Vink <eimertvink@gmail.com>
2018-10-20 17:16:16 +0200

    added descr

--- variables.tf
+++ variables.tf
@@ -1,17 +1,41 @@
-variable "region" {}
-variable "zone" {}
-
-variable "amount" {}
-variable "name_prefix" {}
-variable "machine_type" {}
-variable "user_data" {}
-
-variable "disk_type" {
-  default = "pd-standard" # or pd-ssd
+variable "region" {
+  description = "GC region"
+  default = "europe-west4"
 }
 
-variable "disk_size" {}
-variable "disk_image" {}
+variable "zone" {
+  description = "GC zone"
+  default = "europe-west4-c"
+}
+
+variable "amount" {
+  description = "Number of VMs"
+  default = "1"
+}
+variable "name_prefix" {
+  description = "hostname format: name_prefix-amount"
+  default = "vm"
+}
+variable "machine_type" {
+  description = "List of VM sizes: https://github.com/Eimert/terraform-google-compute-engine-instance#machine_type"
+}
+variable "user_data" {
+  description = "VM description."
+  default = "Managed by terraform plan https://github.com/Eimert/terraform-google-compute-engine-instance"
+}
+
+variable "disk_type" {
+  description = "pd-standard or pd-ssd"
+  default = "pd-standard"
+}
+
+variable "disk_size" {
+  description = "Size in GB"
+  default = "20"
+}
+variable "disk_image" {
+  description = "OS image"
+}
 
 variable "disk_create_local_exec_command_or_fail" {
   default = ":"
@@ -30,26 +54,27 @@ variable "disk_destroy_local_exec_command_and_continue" {
 }
 
 variable "automatic_restart" {
+  description = "Allow google cloud to restart VM for patching (recommended)."
   default = "true"
 }
 
 variable "dns_managed_zone_name_indicator" {
-  description = "descriptive name for dns_zone_name"
+  description = "Descriptive name for dns_zone_name."
   default = "cloud-zone"
 }
 
 variable "dns_zone_name" {
-  # the DNS zone that is pointing to Google' nameservers (if any)
-  # A.k.a. Google' DNS SOA (start of authority)
-  # requires last dot.
-  default = "cloud.example.com."
+  description = "The DNS zone that is pointing to Google' nameservers (if any). A.k.a. Google' DNS SOA (start of authority). Required suffix: . (dot)."
+  default = "cloud.eimertvink.nl."
 }
 
 variable "dns_record_name" {
-  # DNS record type == A
-  # For example: "ansible-dev" will become ansible-dev.cloud.example.com
+  description = "DNS record type == A. For example: "ansible-dev" will become ansible-dev.cloud.example.com"
   default = "ansible-dev"
 }
 
 variable "username" {}
-variable "public_key_path" {}
+variable "public_key_path" {
+  description = "Only private-key auth is enabled by default. Use `ssh-keygen -t rsa` to generate a public-private keypair."
+  default = "~/.ssh/id_rsa.pub"
+}
